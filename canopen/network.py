f232fe8ed0e55864a533ff73aa849cb5e955f3bd
Marcel Kanter <marcel.kanter@googlemail.com>
2019-05-10 11:26:41 +0200

    Added MessageListener and first part of event-chain for CAN messages.

--- canopen/network.py
+++ canopen/network.py
@@ -1,12 +1,35 @@
+import can
+
+
 class Network(object):
 	def __init__(self):
 		self._bus = None
+		self._listeners = [MessageListener(self)]
+		self._notifier = None
 	
 	def connect(self, bus):
 		if self._bus != None:
 			self.disconnect()
 		
 		self._bus = bus
+		self._notifier = can.Notifier(self._bus, self._listeners)
 	
 	def disconnect(self):
+		if self._notifier != None:
+			self._notifier.stop()
+		self._notifier = None
 		self._bus = None
+	
+	def on_message(self, message):
+		pass
+
+
+class MessageListener(can.Listener):
+	def __init__(self, network):
+		if not isinstance(network, Network):
+			raise TypeError()
+		
+		self._network = network
+	
+	def on_message_received(self, message):
+		self._network.on_message(message)
--- test/network_test.py
+++ test/network_test.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import Mock
 import canopen
 import can
 
@@ -8,6 +9,8 @@ class NetworkTestCase(unittest.TestCase):
 		network = canopen.Network()
 		bus = can.Bus(interface = "virtual", channel = 0)
 		
+		network.disconnect()
+		
 		network.connect(bus)
 		
 		network.connect(bus)
@@ -15,6 +18,30 @@ class NetworkTestCase(unittest.TestCase):
 		network.disconnect()
 		
 		bus.shutdown()
+	
+	def test_message(self):
+		network = canopen.Network()
+		message = can.Message(arbitration_id = 0x100, data = [])
+		
+		network.on_message(message)
+
+
+class MessageListenerTestCase(unittest.TestCase):
+	def test_init(self):
+		x = object()
+		with self.assertRaises(TypeError):
+			listener = canopen.network.MessageListener(x)
+	
+	def test_message(self):
+		network = canopen.Network()
+		listener = canopen.network.MessageListener(network)
+		message = can.Message(arbitration_id = 0x100, data = [])
+		
+		network.on_message = Mock()
+		
+		listener.on_message_received(message)
+		
+		network.on_message.assert_called_once_with(message)
 
 
 if __name__ == "__main__":
