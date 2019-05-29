1352b688352db5acc52675403985da16b856494a
Marcel Kanter <marcel.kanter@googlemail.com>
2019-05-29 23:27:02 +0200

    SDO Server request handler Added handler for incoming request messages with decoding of the command. The decoding of the remaining data of the message is not implemented.

--- canopen/node/service/sdo.py
+++ canopen/node/service/sdo.py
@@ -7,6 +7,57 @@ class SDOServer(Service):
 	
 	def attach(self, node):
 		Service.attach(self, node)
+		self._node.network.subscribe(self.on_request, 0x600 + self._node.id)
 	
 	def detach(self):
+		if self._node == None:
+			raise RuntimeError()
+		
+		self._node.network.unsubscribe(self.on_request, 0x600 + self._node.id)
 		Service.detach(self)
+	
+	def on_request(self, message):
+		if message.dlc != 8:
+			return
+		
+		command = message.data[0] & 0xE0
+		if command == 0x00: # Download segment
+			self._on_download_segment(message)
+		if command == 0x20: # Initiate download
+			self._on_initiate_download(message)
+		if command == 0x40: # Initiate upload
+			self._on_initiate_upload(message)
+		if command == 0x60: # Upload segment
+			self._on_upload_segment(message)
+		if command == 0x80: # Abort transfer
+			self._on_abort(message)
+		if command == 0xA0: # Block upload
+			self._on_block_upload(message)
+		if command == 0xC0: # Block download
+			self._on_block_download(message)
+		if command == 0xE0: # Network indication
+			self._on_network_indication(message)
+	
+	def _on_download_segment(self, message):
+		pass
+	
+	def _on_initiate_download(self, message):
+		pass
+	
+	def _on_initiate_upload(self, message):
+		pass
+	
+	def _on_upload_segment(self, message):
+		pass
+	
+	def _on_abort(self, message):
+		pass
+	
+	def _on_block_upload(self, message):
+		pass
+	
+	def _on_block_download(self, message):
+		pass
+	
+	def _on_network_indication(self, message):
+		pass
--- test/node/service/sdo_test.py
+++ test/node/service/sdo_test.py
@@ -1,4 +1,6 @@
 import unittest
+import time
+import can
 import canopen.node.service
 
 
@@ -32,6 +34,57 @@ class SDOServerTestCase(unittest.TestCase):
 		
 		node1.detach()
 		node2.detach()
+
+	def test_request(self):
+		bus = can.Bus(interface = "virtual", channel = 0, receive_own_messages = True)
+		network = canopen.Network()
+		node = canopen.Node("a", 1)
+		sdoserver = canopen.node.service.SDOServer()
+		
+		network.attach(bus)
+		node.attach(network)
+		sdoserver.attach(node)
+		
+		message = can.Message(arbitration_id = 0x601, is_extended_id = False, data = b"\x00")
+		bus.send(message)
+		time.sleep(0.001)
+		
+		message = can.Message(arbitration_id = 0x601, is_extended_id = False, data = b"\x00\x00\x00\x00\x00\x00\x00\x00")
+		bus.send(message)
+		time.sleep(0.001)
+		
+		message = can.Message(arbitration_id = 0x601, is_extended_id = False, data = b"\x20\x00\x00\x00\x00\x00\x00\x00")
+		bus.send(message)
+		time.sleep(0.001)
+		
+		message = can.Message(arbitration_id = 0x601, is_extended_id = False, data = b"\x40\x00\x00\x00\x00\x00\x00\x00")
+		bus.send(message)
+		time.sleep(0.001)
+		
+		message = can.Message(arbitration_id = 0x601, is_extended_id = False, data = b"\x60\x00\x00\x00\x00\x00\x00\x00")
+		bus.send(message)
+		time.sleep(0.001)
+		
+		message = can.Message(arbitration_id = 0x601, is_extended_id = False, data = b"\x80\x00\x00\x00\x00\x00\x00\x00")
+		bus.send(message)
+		time.sleep(0.001)
+		
+		message = can.Message(arbitration_id = 0x601, is_extended_id = False, data = b"\xA0\x00\x00\x00\x00\x00\x00\x00")
+		bus.send(message)
+		time.sleep(0.001)
+		
+		message = can.Message(arbitration_id = 0x601, is_extended_id = False, data = b"\xC0\x00\x00\x00\x00\x00\x00\x00")
+		bus.send(message)
+		time.sleep(0.001)
+		
+		message = can.Message(arbitration_id = 0x601, is_extended_id = False, data = b"\xE0\x00\x00\x00\x00\x00\x00\x00")
+		bus.send(message)
+		time.sleep(0.001)
+		
+		sdoserver.detach()
+		node.detach()
+		network.detach()
+		bus.shutdown()
 
 
 if __name__ == "__main__":
