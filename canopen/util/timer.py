17147248253d8bda7d47eace60f8da88ddedf3c3
Marcel Kanter <marcel.kanter@googlemail.com>
2019-07-29 20:54:05 +0200

    Timer calls function after interval has elapsed

--- canopen/util/timer.py
+++ canopen/util/timer.py
@@ -2,7 +2,9 @@ import threading
 
 
 class Timer(threading.Thread):
-	def __init__(self, function, args = None, kwargs = None):
+	def __init__(self, interval, function, args = None, kwargs = None):
+		if interval < 0:
+			raise ValueError()
 		if not callable(function):
 			raise ValueError()
 		if args == None:
@@ -12,26 +14,33 @@ class Timer(threading.Thread):
 		
 		threading.Thread.__init__(self)
 		
+		self._interval = interval
 		self._function = function
 		self._args = args
 		self._kwargs = kwargs
 		
 		self._terminate = threading.Event()
 		self._trigger = threading.Event()
+		self._condition = threading.Event()
 		
 		threading.Thread.start(self)
 	
-	def start(self):
+	def start(self, interval = None):
+		if interval != None:
+			self._interval = interval
 		self._trigger.set()
 	
 	def run(self):
 		self._trigger.wait()
 		self._trigger.clear()
 		while not self._terminate.is_set():
-			self._function(*self._args, **self._kwargs)
+			self._condition.wait(self._interval)
+			if not self._condition.is_set():
+				self._function(*self._args, **self._kwargs)
 			self._trigger.wait()
 			self._trigger.clear()
 	
 	def stop(self):
 		self._terminate.set()
+		self._condition.set()
 		self._trigger.set()
--- tests/util/timer_test.py
+++ tests/util/timer_test.py
@@ -7,24 +7,27 @@ import canopen.util
 class TimerTest(unittest.TestCase):
 	def test_init(self):
 		with self.assertRaises(ValueError):
-			canopen.util.Timer(None)
+			canopen.util.Timer(1.0, None)
 		
 		m = Mock()
-		examinee = canopen.util.Timer(m)
+		with self.assertRaises(ValueError):
+			canopen.util.Timer(-1.0, m)
+		
+		examinee = canopen.util.Timer(1.0, m)
 		self.assertTrue(examinee.is_alive())
 		
 		examinee.stop()
 		time.sleep(0.01)
 		self.assertFalse(examinee.is_alive())
 		
-		examinee = canopen.util.Timer(m, [10])
+		examinee = canopen.util.Timer(1.0, m, [10])
 		self.assertTrue(examinee.is_alive())
 		
 		examinee.stop()
 		time.sleep(0.01)
 		self.assertFalse(examinee.is_alive())
 		
-		examinee = canopen.util.Timer(m, [10], {"a": 20})
+		examinee = canopen.util.Timer(1.0, m, [10], {"a": 20})
 		self.assertTrue(examinee.is_alive())
 		
 		examinee.stop()
@@ -33,22 +36,34 @@ class TimerTest(unittest.TestCase):
 	
 	def test_all(self):
 		m = Mock()
-		examinee = canopen.util.Timer(m, [10], {"a": 20})
-		time.sleep(0.1)
+		examinee = canopen.util.Timer(0.1, m, [10], {"a": 20})
+		
+		#### Test step: The timer should not start immediately after init
+		time.sleep(0.2)
 		m.assert_not_called()
 		
-		examinee.start()
-		time.sleep(0.1)
-		m.assert_called()
+		#### Test step: Start with interval from init
 		m.reset_mock()
-		
 		examinee.start()
-		time.sleep(0.1)
+		time.sleep(0.08)
+		m.assert_not_called()
+		time.sleep(0.04)
 		m.assert_called()
-		m.reset_mock()
 		
+		#### Test step: Overwrite interval with new value
+		m.reset_mock()
+		examinee.start(0.2)
+		time.sleep(0.18)
+		m.assert_not_called()
+		time.sleep(0.04)
+		m.assert_called()
+		
+		#### Test step: Stop, no call at the end
+		m.reset_mock()
+		examinee.start(0.2)
+		time.sleep(0.1)
 		examinee.stop()
-		time.sleep(0.1)
+		time.sleep(0.2)
 		m.assert_not_called()
 		
 		self.assertFalse(examinee.is_alive())
