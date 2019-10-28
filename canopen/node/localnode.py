66b3f210514cf76dc9b10e56ec634d4e659f8638
Marcel Kanter <marcel.kanter@googlemail.com>
2019-10-28 09:38:16 +0100

    Added rpdo and tpdo to LocalNode and RemoteNode to complete the generic default connection set. Indexing starts with 1, so rpdo[1] is RPDO1, etc.

--- canopen/node/localnode.py
+++ canopen/node/localnode.py
@@ -1,6 +1,6 @@
 import canopen.objectdictionary
 from .node import Node
-from .service import NMTSlave, EMCYProducer, SDOServer
+from .service import NMTSlave, EMCYProducer, SDOServer, PDOConsumer, PDOProducer
 
 
 class LocalNode(Node):
@@ -14,6 +14,8 @@ class LocalNode(Node):
 		self.nmt = NMTSlave()
 		self.emcy = EMCYProducer()
 		self.sdo = SDOServer()
+		self.rpdo = {1: PDOConsumer(), 2: PDOConsumer(), 3: PDOConsumer(), 4: PDOConsumer()}
+		self.tpdo = {1: PDOProducer(), 2: PDOProducer(), 3: PDOProducer(), 4: PDOProducer()}
 	
 	def attach(self, network):
 		""" Attach the node and then all services to the network. It does NOT add or assign the node to the network."""
@@ -21,9 +23,21 @@ class LocalNode(Node):
 		self.nmt.attach(self)
 		self.sdo.attach(self)
 		self.emcy.attach(self)
+		self.tpdo[1].attach(self, 0x180 + self._id)
+		self.tpdo[2].attach(self, 0x280 + self._id)
+		self.tpdo[3].attach(self, 0x380 + self._id)
+		self.tpdo[4].attach(self, 0x480 + self._id)
+		self.rpdo[1].attach(self, 0x200 + self._id)
+		self.rpdo[2].attach(self, 0x300 + self._id)
+		self.rpdo[3].attach(self, 0x400 + self._id)
+		self.rpdo[4].attach(self, 0x500 + self._id)
 	
 	def detach(self):
 		""" Detaches all services and then the node from the network. It does NOT remove or delete the node from the network."""
+		for i in self.rpdo:
+			self.rpdo[i].detach()
+		for i in self.tpdo:
+			self.tpdo[i].detach()
 		self.emcy.detach()
 		self.sdo.detach()
 		self.nmt.detach()
--- canopen/node/remotenode.py
+++ canopen/node/remotenode.py
@@ -1,5 +1,5 @@
 from .node import Node
-from .service import NMTMaster, EMCYConsumer, SDOClient
+from .service import NMTMaster, EMCYConsumer, SDOClient, PDOConsumer, PDOProducer
 
 
 class RemoteNode(Node):
@@ -12,6 +12,8 @@ class RemoteNode(Node):
 		self.nmt = NMTMaster()
 		self.emcy = EMCYConsumer()
 		self.sdo = SDOClient()
+		self.rpdo = {1: PDOProducer(), 2: PDOProducer(), 3: PDOProducer(), 4: PDOProducer()}
+		self.tpdo = {1: PDOConsumer(), 2: PDOConsumer(), 3: PDOConsumer(), 4: PDOConsumer()}
 	
 	def attach(self, network):
 		""" Attach the node and then all services to the network. It does NOT add or assign the node to the network."""
@@ -19,9 +21,21 @@ class RemoteNode(Node):
 		self.nmt.attach(self)
 		self.sdo.attach(self)
 		self.emcy.attach(self)
+		self.tpdo[1].attach(self, 0x180 + self._id)
+		self.tpdo[2].attach(self, 0x280 + self._id)
+		self.tpdo[3].attach(self, 0x380 + self._id)
+		self.tpdo[4].attach(self, 0x480 + self._id)
+		self.rpdo[1].attach(self, 0x200 + self._id)
+		self.rpdo[2].attach(self, 0x300 + self._id)
+		self.rpdo[3].attach(self, 0x400 + self._id)
+		self.rpdo[4].attach(self, 0x500 + self._id)
 	
 	def detach(self):
 		""" Detaches all services and then the node from the network. It does NOT remove or delete the node from the network."""
+		for i in self.rpdo:
+			self.rpdo[i].detach()
+		for i in self.tpdo:
+			self.tpdo[i].detach()
 		self.emcy.detach()
 		self.sdo.detach()
 		self.nmt.detach()
