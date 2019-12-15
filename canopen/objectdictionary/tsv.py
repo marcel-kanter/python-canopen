27b83a466ba7ca7d524ceb5d8f42e88f3b50fb9c
Marcel Kanter <marcel.kanter@googlemail.com>
2019-12-15 11:37:28 +0100

    Change column names to UpperCamelCase similar to the key names of the EDS file format.

--- canopen/objectdictionary/tsv.py
+++ canopen/objectdictionary/tsv.py
@@ -3,16 +3,16 @@ import canopen.objectdictionary
 
 
 def _dump_obj(obj, writer):
-	r = {"index": hex(obj.index), "name": obj.name, "object type": hex(obj.object_type), "description": obj.description}
+	r = {"Index": hex(obj.index), "Name": obj.name, "ObjectType": hex(obj.object_type), "Description": obj.description}
 	
 	if isinstance(obj, (canopen.objectdictionary.Variable, canopen.objectdictionary.Array, canopen.objectdictionary.Record)) and not isinstance(obj, (canopen.objectdictionary.DefStruct, canopen.objectdictionary.Domain)):
-		r["data type"] = hex(obj.data_type)
+		r["DataType"] = hex(obj.data_type)
 	
 	if isinstance(obj, canopen.objectdictionary.Variable):
-		r["access type"] = obj.access_type
+		r["AccessType"] = obj.access_type
 	
 	if isinstance(obj, canopen.objectdictionary.Variable) and not isinstance(obj, canopen.objectdictionary.Domain):
-		r["sub-index"] = hex(obj.subindex)
+		r["SubIndex"] = hex(obj.subindex)
 	
 	writer.writerow(r)
 	
@@ -22,7 +22,7 @@ def _dump_obj(obj, writer):
 
 
 def dump(obj, file):
-	field_names = ["index", "name", "object type", "sub-index", "data type", "access type", "description"]
+	field_names = ["Index", "Name", "ObjectType", "SubIndex", "DataType", "AccessType", "Description"]
 	writer = csv.DictWriter(file, field_names, delimiter = "\t")
 	
 	writer.writeheader()
@@ -38,32 +38,32 @@ def load(file):
 	reader = csv.DictReader(file, delimiter = "\t")
 	
 	for row in reader:
-		object_type = int(row["object type"], 16)
-		name = row["name"]
-		index = int(row["index"], 16)
+		object_type = int(row["ObjectType"], 16)
+		name = row["Name"]
+		index = int(row["Index"], 16)
 		
 		if object_type == 6:
 			o = canopen.objectdictionary.DefStruct(name, index)
 			dictionary.add(o)
 		if object_type == 8:
-			data_type = int(row["data type"], 16)
+			data_type = int(row["DataType"], 16)
 			o = canopen.objectdictionary.Array(name, index, data_type)
 			dictionary.add(o)
 		if object_type == 9:
-			data_type = int(row["data type"], 16)
+			data_type = int(row["DataType"], 16)
 			o = canopen.objectdictionary.Record(name, index, data_type)
 			dictionary.add(o)
 		if object_type == 2:
-			access_type = row["access type"]
+			access_type = row["AccessType"]
 			o = canopen.objectdictionary.Domain(name, index, access_type)
 			dictionary.add(o)
 		if object_type == 5:
 			o = canopen.objectdictionary.DefType(name, index)
 			dictionary.add(o)
 		if object_type == 7:
-			subindex = int(row["sub-index"], 16)
-			data_type = int(row["data type"], 16)
-			access_type = row["access type"]
+			subindex = int(row["SubIndex"], 16)
+			data_type = int(row["DataType"], 16)
+			access_type = row["AccessType"]
 			o = canopen.objectdictionary.Variable(name, index, subindex, data_type, access_type)
 			# If there is already an object in the dictionary asume it is an Array of Record
 			if index in dictionary:
