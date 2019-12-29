f5a676b2a9f3813504fb6ca657b31ad3b9990ca6
Marcel Kanter <marcel.kanter@googlemail.com>
2019-12-29 20:22:52 +0100

    Fix #3: Classifiers are missing in build script Added classifiers

--- setup.py
+++ setup.py
@@ -13,5 +13,26 @@ setup(
 	setup_requires = ["pytest-runner"],
 	tests_require = ["pytest", "pytest-timeout", "pytest-cov", "pytest-subtests"],
 	
-	url = "https://github.com/marcel-kanter/python-canopen"
+	url = "https://github.com/marcel-kanter/python-canopen",
+	
+	classifiers = [
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Education",
+		"Intended Audience :: Healthcare Industry",
+		"Intended Audience :: Information Technology",
+		"Intended Audience :: Manufacturing",
+		"Intended Audience :: Science/Research",
+		"Intended Audience :: Telecommunications Industry",
+		"License :: Other/Proprietary License",
+		"Operating System :: MacOS",
+		"Operating System :: Microsoft :: Windows",
+		"Operating System :: POSIX :: Linux",
+		"Programming Language :: Python",
+		"Topic :: System :: Networking",
+		"Topic :: Software Development",
+		"Topic :: Software Development :: Testing",
+		"Topic :: Software Development :: Embedded Systems",
+		"Topic :: Utilities"
+		]
 )
