7d1700e4bba60d28ad4f931dcdd325b938f64905
Marcel Kanter <marcel.kanter@googlemail.com>
2020-01-05 15:48:07 +0100

    Append and getitem method for ObjectMapping

--- canopen/node/service/objectmapping.py
+++ canopen/node/service/objectmapping.py
@@ -2,5 +2,37 @@ class ObjectMapping(object):
 	def __init__(self):
 		self._items = []
 	
+	def __getitem__(self, slot):
+		""" Gets a variable from the mapping.
+		Raises a KeyError if the slot is unused.
+		
+		:param slot: The slot from with the variable should be returned. Must be a non-negativer integer lower than the number of mapped variables.
+		
+		:returns: A tuple in the form of (entry, length) with entry beeing a Variable or a tuple in the form of (index, subindex)
+		
+		:raises: IndexError
+		"""
+		return self._items[slot]
+	
 	def __len__(self):
+		""" Gets the number of mapped variables.
+		
+		:returns: number of mapped variables
+		"""
 		return len(self._items)
+	
+	def append(self, variable, length):
+		""" Appends a variable to the mapping, only length bits should be used.
+		
+		:param variable: Variable to append. Must be a Variable from object dictionary, a Variable from node or a tuple in the form of (index, subindex).
+		
+		:param length: Number of bits to use. Must be a non-negative integer.
+		
+		:raises: TypeError, ValueError
+		"""
+		if int(length) < 0:
+			raise ValueError()
+		
+		entry = variable
+		
+		self._items.append((entry, length))
--- tests/node/service/test_objectmapping.py
+++ tests/node/service/test_objectmapping.py
@@ -1,4 +1,5 @@
 import unittest
+import canopen.objectdictionary
 from canopen.node.service.objectmapping import ObjectMapping
 
 
@@ -6,6 +7,30 @@ class ObjectMappingTest(unittest.TestCase):
 	def test_init(self):
 		examinee = ObjectMapping()
 		self.assertEqual(len(examinee), 0)
+	
+	def test_append(self):
+		d = canopen.ObjectDictionary()
+		d.add(canopen.objectdictionary.Variable("var", 0x2000, 0x00, canopen.objectdictionary.INTEGER32))
+		
+		examinee = ObjectMapping()
+		
+		with self.assertRaises(ValueError):
+			examinee.append((0x1000, 0x00), -1)
+		
+		with self.assertRaises(IndexError):
+			examinee[0]
+		
+		examinee.append((0x1000, 0x00), 0)
+		self.assertEqual(len(examinee), 1)
+		self.assertEqual(examinee[0], ((0x1000, 0x00), 0))
+		
+		examinee.append((0x1000, 0x00), 1)
+		self.assertEqual(len(examinee), 2)
+		self.assertEqual(examinee[1], ((0x1000, 0x00), 1))
+		
+		examinee.append(d["var"], 10)
+		self.assertEqual(len(examinee), 3)
+		self.assertEqual(examinee[2], (d["var"], 10))
 
 
 if __name__ == "__main__":
